6347ac2592f1cbd4eb410f9f8be10bb7f6b868a1
Dully <133727755+DullSmallmega176@users.noreply.github.com>
2025-10-10 01:06:06 -0500

    Reworked detection + added more detail. thank you windows for releasing 25h2

--- termsrv_rdp_patch.ps1
+++ termsrv_rdp_patch.ps1
@@ -2,25 +2,44 @@
 # Details here http://woshub.com/how-to-allow-multiple-rdp-sessions-in-windows-10/
 
 # Checking OS version
+Write-Output "Checking windows version..."
 $osInfo = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-$releaseId = $osInfo.releaseId
 $currentBuild = $osInfo.currentBuild
 $displayVersion = $osInfo.displayVersion
-Write-Output "Detected Windows Version: $displayVersion (Build $currentBuild)"
-if ($displayVersion -eq "24H2") {
-    Write-Output "This script does NOT support Windows 24H2. Exiting to avoid any system damage or corruption, exiting"
+if (-not $displayVersion) {
+    Write-Host "Error: Could not determine Windows version." -ForegroundColor Red
+    Write-Host "For your safety, this script will close in 5 seconds."
     Start-Sleep -Seconds 5
     Exit
 }
+Write-Output "Detected Windows Version: $displayVersion (Build $currentBuild)"
+# Check if the OS version is a safe one to do the patch on.
+if ($displayVersion -match '^\d{2}H\d$') {
+    $version= [int]($displayVersion -replace 'H', '')
+    if ($version -ge 242) {
+        Write-Host "Error: Your Windows version ($displayVersion) is NOT supported." -ForegroundColor Red
+        Write-Host "For your safety, this script will close in 5 seconds."
+        Start-Sleep -Seconds 5
+        Exit
+    } else {
+        Write-Output "Windows version is supported, proceeding."
+    }
+} else {
+    Write-Output "Seems like your windows version format is weird. (not ##H# 'e.g. 24H2')"
+}
 # Stop RDP service, make a backup of the termsrv.dllfile and change the permissions 
+Write-Output "Stopping Remote Desktop Services..."
 Stop-Service UmRdpService -Force
 Stop-Service TermService -Force
+Write-Output "Making a copy termsrv.dll as termsrv.dll.copy"
 $termsrv_dll_acl = Get-Acl c:\windows\system32\termsrv.dll
 Copy-Item c:\windows\system32\termsrv.dll c:\windows\system32\termsrv.dll.copy
+Write-Output "Taking ownership of termsrv.dll"
 takeown /f c:\windows\system32\termsrv.dll
 $new_termsrv_dll_owner = (Get-Acl c:\windows\system32\termsrv.dll).owner
 cmd /c "icacls c:\windows\system32\termsrv.dll /Grant $($new_termsrv_dll_owner):F /C"
 # search for a pattern in termsrv.dll file 
+Write-Output "Searching for the string pattern to patch termsrv.dll"
 $dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -Encoding byte
 $dll_as_text = $dll_as_bytes.forEach('ToString', 'X2') -join ' '
 $patternregex = ([regex]'39 81 3C 06 00 00(\s\S\S){6}')
@@ -30,14 +49,19 @@ If ($checkPattern -ne $null) {
     $dll_as_text_replaced = $dll_as_text -replace $patternregex, $patch
 }
 Elseif (Select-String -Pattern $patch -InputObject $dll_as_text) {
+    Write-Output "termsrv.dll file is already patched, exiting script."
+    Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
     Start-Sleep -Seconds 5
-    Write-Output 'The termsrv.dll file is already patched, exiting'
     Exit
 }
 else { 
-    Write-Output "Pattern not found "
+    Write-Output "Pattern not found, exiting script."
+    Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
+    Start-Sleep -Seconds 5
+    Exit
 }
 # patching termsrv.dll
+Write-Output "Pattern was found, continuing to patching termsrv.dll"
 [byte[]] $dll_as_bytes_replaced = -split $dll_as_text_replaced -replace '^', '0x'
 Set-Content c:\windows\system32\termsrv.dll.patched -Encoding Byte -Value $dll_as_bytes_replaced
 # comparing two files 
@@ -47,3 +71,6 @@ Copy-Item c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dl
 Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
 Start-Service UmRdpService
 Start-Service TermService
+Write-Host "Patch was successful!!!" -ForegroundColor Green
+Start-Sleep -Seconds 3
+Exit
